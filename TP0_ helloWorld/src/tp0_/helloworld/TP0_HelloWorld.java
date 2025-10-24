a8ee5ac535918412e9a7ce9de44d0a254e25e601
Mathieu  Johnson <mathieu.johnson75@gmail.com>
2025-10-24 10:11:50 +0200

    création d'un nouveau projet versionné

---
+++ TP0_ helloWorld/src/tp0_/helloworld/TP0_HelloWorld.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_.helloworld;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author mathieu
+ * 20/10/2025
+ */
+public class TP0_HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Scanner sc ; 
+    sc = new Scanner (System.in) ; 
+    System.out.println("Quel est votre prénom ?"); 
+    sc.nextLine();
+    }
+    
+}
